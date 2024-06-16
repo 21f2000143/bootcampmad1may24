e7bbd622e852d33571af36a3bdf00eee9665f423
Sachin Kumar <pseudo-01@outlook.com>
2024-06-16 20:39:00 +0530

    added flask-login

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ This repo is to create mad1 grocery app to show the demonstration.
 ## Just testing if my local repo is connected with the github repo
 ## guided student with fetching the changes
 # tutorial for fectching changes from master to mydev
+
+## to activate the virtual environment on cmd prompt
+.env\Scripts\activate 
+
+### link for flask-login
+[Flask login](https://pypi.org/project/Flask-Login/)
--- main.py
+++ main.py
@@ -1,34 +1,32 @@
 from flask import Flask, render_template
-from database import db
-from models import Product
+from application.database import db
+from application.models import *
+from flask_login import LoginManager
+
+
+login_manager = LoginManager()
+
+
+# I will explain later
+@login_manager.user_loader
+def load_user(id):
+    return User.query.filter_by(id=id).first()
+
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = 'jjirfar45uqlga@35qkmf'
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite3"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+    login_manager.init_app(app)
     app.app_context().push()
     db.create_all()
     return app
 
 
 app = create_app()
-
-
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
-
-
-@app.route("/myprofile")
-def my_profile():
-    return render_template("profile.html")
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
+from application.userControllers import *
 
 if __name__ == "__main__":
     app.run(debug=True)
